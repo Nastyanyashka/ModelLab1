6a1afd60e3f5a224cac314e57348ba633b304c00
aleksandr_help@mail.ru <aleksandr_help@mail.ru>
2023-05-14 15:03:00 +0700

    Добавьте файлы проекта.

---
+++ ModelLab1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ModelLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.generateAndSolve = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(29, 12);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(485, 305);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(573, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(749, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // generateAndSolve
+            // 
+            this.generateAndSolve.Location = new System.Drawing.Point(548, 275);
+            this.generateAndSolve.Name = "generateAndSolve";
+            this.generateAndSolve.Size = new System.Drawing.Size(117, 20);
+            this.generateAndSolve.TabIndex = 3;
+            this.generateAndSolve.Text = "button1";
+            this.generateAndSolve.UseVisualStyleBackColor = true;
+            this.generateAndSolve.Click += new System.EventHandler(this.GenerateAndSolve_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 572);
+            this.Controls.Add(this.generateAndSolve);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button generateAndSolve;
+    }
+}
+
---
+++ ModelLab1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace ModelLab1
+{
+    public partial class Form1 : Form
+    {
+        const double limit = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series["Series1"]["PixelPointWidth"] = "14";
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+        }
+
+        double Func(double l, double x)
+        {
+            return 2 - 2 * Math.Sqrt(1 - x);
+        }
+
+        double dplus(double[] num)
+        {
+            double[] d1 = new double[num.Length];
+            int n = num.Length;
+            for (int i = 0; i < num.Length; i++)
+            {
+                d1[i] = ((i+1) / (double)n) - num[i];
+            }
+            return d1.Max();
+
+        }
+
+        double dminus(double[] num)
+        {
+            double[] d2 = new double[num.Length];
+            int n = num.Length;
+            for (int i = 0; i < num.Length; i++)
+            {
+                d2[i] = num[i] - (i) / (double)n;
+            }
+            return d2.Max();
+
+        }
+
+        private void GenerateAndSolve_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            chart1.Series.Add(new Series());
+            Random r1 = new Random();
+
+            const int amountOfNum = 1000, amountOfIntervals = 10;
+            double[] nums = new double[amountOfNum];
+            double leftMargin, rightMargin;
+            int counter;
+            for (int i = 0; i < amountOfNum; i++)
+            {
+                nums[i] = Func(1, r1.NextDouble());
+            }
+            double iterator = limit / amountOfIntervals;
+            leftMargin = 0;
+            for (rightMargin = iterator; rightMargin <= 2.1; rightMargin += iterator)
+            {
+                counter = 0;
+                foreach (double i in nums)
+                {
+                    if (i < rightMargin && i >= leftMargin)
+                        counter++;
+                }
+
+                chart1.Series[0].Points.AddXY(leftMargin, counter / (double)amountOfNum);
+                leftMargin = rightMargin;
+            }
+            double d1max = dplus(nums);
+            double d2max = dminus(nums);
+            double d = 0;
+            if (d1max > d2max)
+            {
+                d = d1max;
+            }
+            else { d = d2max; }
+           double kvantil = Math.Sqrt(-0.5*Math.Log((1-0.995)/2));
+            if (radioButton1.Checked == true && d <= kvantil)
+            {
+                label1.Text = "Гипотеза доказана";
+            }
+            else { label1.Text = "Гипотеза не доказана"; }
+        }
+    }
+}
